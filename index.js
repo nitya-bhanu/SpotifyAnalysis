5d6cc1cdb610d2f51b64a0709622aeb228b4ace8
Nitya Bhanu Gogawat <nityabhanu2001@gmail.com>
2023-05-02 01:29:27 +0530

    added web files

---
+++ index.js
@@ -0,0 +1,120 @@
+function myFunction1() {
+  var x = document.getElementById("vis-1");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+  var x1 = document.getElementById("vis-1");
+  var x2 = document.getElementById("vis-2");
+  var x3 = document.getElementById("vis-3");
+  var x4 = document.getElementById("vis-4");
+  var x5 = document.getElementById("vis-5");
+  var x6 = document.getElementById("vis-6");
+  // x1.style.display="none";
+  x2.style.display="none";
+  x3.style.display="none";
+  x4.style.display="none";
+  x5.style.display="none";
+  x6.style.display="none";
+}
+function myFunction2() {
+    var x = document.getElementById("vis-2");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  var x1 = document.getElementById("vis-1");
+  var x2 = document.getElementById("vis-2");
+  var x3 = document.getElementById("vis-3");
+  var x4 = document.getElementById("vis-4");
+  var x5 = document.getElementById("vis-5");
+  var x6 = document.getElementById("vis-6");
+  x1.style.display="none";
+  // x2.style.display="none";
+  x3.style.display="none";
+  x4.style.display="none";
+  x5.style.display="none";
+  x6.style.display="none";
+  }
+  function myFunction3() {
+    var x = document.getElementById("vis-3");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+    var x1 = document.getElementById("vis-1");
+    var x2 = document.getElementById("vis-2");
+    var x3 = document.getElementById("vis-3");
+    var x4 = document.getElementById("vis-4");
+    var x5 = document.getElementById("vis-5");
+    var x6 = document.getElementById("vis-6");
+    x1.style.display="none";
+    x2.style.display="none";
+    // x3.style.display="none";
+    x4.style.display="none";
+    x5.style.display="none";
+    x6.style.display="none";
+  }
+  function myFunction4() {
+    var x = document.getElementById("vis-4");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+    var x1 = document.getElementById("vis-1");
+    var x2 = document.getElementById("vis-2");
+    var x3 = document.getElementById("vis-3");
+    var x4 = document.getElementById("vis-4");
+    var x5 = document.getElementById("vis-5");
+    var x6 = document.getElementById("vis-6");
+    x1.style.display="none";
+    x2.style.display="none";
+    x3.style.display="none";
+    // x4.style.display="none";
+    x5.style.display="none";
+    x6.style.display="none";
+  }
+  function myFunction5() {
+    var x = document.getElementById("vis-5");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+    var x1 = document.getElementById("vis-1");
+    var x2 = document.getElementById("vis-2");
+    var x3 = document.getElementById("vis-3");
+    var x4 = document.getElementById("vis-4");
+    var x5 = document.getElementById("vis-5");
+    var x6 = document.getElementById("vis-6");
+    x1.style.display="none";
+    x2.style.display="none";
+    x3.style.display="none";
+    x4.style.display="none";
+    // x5.style.display="none";
+    x6.style.display="none";
+  }
+  function myFunction6() {
+    var x = document.getElementById("vis-6");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+    var x1 = document.getElementById("vis-1");
+    var x2 = document.getElementById("vis-2");
+    var x3 = document.getElementById("vis-3");
+    var x4 = document.getElementById("vis-4");
+    var x5 = document.getElementById("vis-5");
+    var x6 = document.getElementById("vis-6");
+    x1.style.display="none";
+    x2.style.display="none";
+    x3.style.display="none";
+    x4.style.display="none";
+    x5.style.display="none";
+    // x6.style.display="none";
+  }
